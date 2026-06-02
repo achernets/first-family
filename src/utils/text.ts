2594dd6b88d90fe67e1dff9eab27b0bca3eeca93
o.chernets <chernetsoleksandr.dev@gmail.com>
2026-06-02 10:41:24 +0300

    translates

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,7 +1,7 @@
-export const REQUIRE_TEXT = `Обовз\'якове поле`;
-export const SERVER_ERROR_TEXT = 'Помилка сервера';
-export const FORM_DARA_ERROR_TEXT = 'Перевірте заповненість данних';
-export const UNIQUE_FIELD_TEXT = 'Поле має бути унікальним';
-export const PARSE_FILTERS_STRING_TEXT_ERROR = 'Помилка розбору фільтра';
-export const USER_OR_PASSWORD_ERROR = 'Невірний користувач або пароль';
-export const NO_ACCESS = 'Немає доступу';
+export const REQUIRE_TEXT = 'This field is required';
+export const SERVER_ERROR_TEXT = 'Server error';
+export const FORM_DARA_ERROR_TEXT = 'Please check the form data';
+export const UNIQUE_FIELD_TEXT = 'This field must be unique';
+export const PARSE_FILTERS_STRING_TEXT_ERROR = 'Failed to parse filters';
+export const USER_OR_PASSWORD_ERROR = 'Invalid username or password';
+export const NO_ACCESS = 'You do not have access';
